94b8c6465595b0121b8c3a35e0d9c9cf5bfc06a3
khaoulakanboua <khaoulakanboua@gmail.com>
2023-12-30 20:15:12 +0100

    commit

--- src/test/java/com/example/demo/DevoireApplicationTests.java
+++ src/test/java/com/example/demo/DevoireApplicationTests.java
@@ -1,7 +1,18 @@
 package com.example.demo;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.example.demo.security.springjwt.models.User;
+import com.example.demo.security.springjwt.repository.UserRepository;
+import com.example.demo.security.springjwt.security.jwt.services.UserService;
 
 @SpringBootTest
 class DevoireApplicationTests {
@@ -10,4 +21,6 @@ class DevoireApplicationTests {
 	void contextLoads() {
 	}
 
+
+	
 }
